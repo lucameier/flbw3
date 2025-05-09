6808e2249ee50e654d7a53b213b1e9e6f96efda7
Luca <kontakt@lucameier.ch>
2025-05-09 07:28:15 +0000

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -212,7 +212,7 @@ def transform_data(file_buffer):
     static_cols2 = [
         "Organisationseinheit", "U-Nummer", "Name", "Kontierungsbeschreibung",
         "Kontierungstyp", "Kontierungsnummer", "Leistung Kurztext", "Leistungsart",
-        "Kontierungstyp", "Kontierungsnummer", 'Leistung Kurztext', 'Leistungsart', 'Kategorie', 'Unterkategorie', 'Unterkategorie Name'
+        "Kategorie", "Unterkategorie", "Unterkategorie Name"
     ]
     
     # Pivotierung: Aggregiere den Betrag pro Gruppe (definiert durch die statischen Felder) und Monat
